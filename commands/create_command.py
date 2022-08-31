88949c1f67ee646adadd98c30e0d19e5fa699de8
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2022-09-01 01:50:23 +0200

    Update create_command.py

--- commands/create_command.py
+++ commands/create_command.py
@@ -1,8 +1,12 @@
+import json
+import os
+import sys
+from Schemakeys import Schema_Keys
 from Icommand import ICommand
-import os, json, sys
+
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.append(parent_dir)
-from SchemaKeys import SchemaKeys
+
 
 class CreateCommand(ICommand):
     def __init__(self, schema):
@@ -10,20 +14,20 @@ class CreateCommand(ICommand):
         self.data = json.load(open(schema, "r"))
 
     def execute(self):
-        if (self.schema == None or not os.path.exists(os.path.join(parent_dir, self.schema))):
+        if self.schema is None or not os.path.exists(os.path.join(parent_dir, self.schema)):
             raise Exception("FileNotFound")
-        
+
         if not SchemaKeys().DATABASE in self.data:
             raise Exception("DatabaseNameIsMissing")
-            
-        dir = self.data[SchemaKeys().DATABASE]
+
+        dir = self.data[Schema_Keys().DATABASE]
         path = os.path.join(parent_dir, dir)
-        
-        os.makedirs(path, exist_ok = True)
-        
-        if not SchemaKeys().TABLES in self.data:
+
+        os.makedirs(path, exist_ok=True)
+
+        if not Schema_Keys().TABLES in self.data:
             return
-        
-        for table in self.data[SchemaKeys().TABLES]:
-            t_path = os.path.join(path, table[SchemaKeys().NAME])
-            os.makedirs(t_path, exist_ok = True)
+
+        for table in self.data[Schema_Keys().TABLES]:
+            t_path = os.path.join(path, table[Schema_Keys().NAME])
+            os.makedirs(t_path, exist_ok=True)
